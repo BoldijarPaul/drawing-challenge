fc657a23902d479113a4b61b298cad8c9fe4c349
Boldijar Paul <boldijar.paul@gmail.com>
2016-10-23 22:28:56 +0300

    initial commit

---
+++ BaseDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawing_challenge
+{
+    class BaseDrawer
+    {
+        public virtual Bitmap generate()
+        {
+            Bitmap bitmap = new Bitmap(500,500);
+            for(int i=0; i<bitmap.Width; i++)
+            {
+                for (int j=0; j < bitmap.Height; j++)
+                {
+                    Color color = (i%2==0 && j%2==0) ? Color.Red : Color.Purple;
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+            return bitmap;
+        }
+    }
+}
---
+++ BlackWhiteDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drawing_challenge
+{
+    class BlackWhiteDrawer : BaseDrawer
+    {
+        public override Bitmap generate()
+        {
+            Bitmap bitmap = new Bitmap(500, 500);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Color color = (Math.Abs(i-j)<20) ? Color.White : Color.Black;
+                    bitmap.SetPixel(i, j, color);
+                }
+            }
+            return bitmap;
+        }
+    }
+}
